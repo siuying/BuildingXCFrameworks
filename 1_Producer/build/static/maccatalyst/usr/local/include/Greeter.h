8d570b468a00769f537ed8a47b778123e4c8d024
Francis Chong <siu.ying@gmail.com>
2020-02-09 16:41:49 +0800

    add build folder

---
+++ 1_Producer/build/static/maccatalyst/usr/local/include/Greeter.h
@@ -0,0 +1,16 @@
+//
+//  Greeter.h
+//  SampleStatic
+//
+//  Created by Francis Chong on 9/2/2020.
+//  Copyright © 2020 Francis Chong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface Greeter : NSObject
+
++ (NSString*) greeting;
+
+@end
+NS_ASSUME_NONNULL_END
