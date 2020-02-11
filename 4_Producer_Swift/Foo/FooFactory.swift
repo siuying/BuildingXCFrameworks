b21f6965496c0806fc042464f4a8deb601fc0949
Francis Chong <siu.ying@gmail.com>
2020-02-12 01:15:59 +0800

    Add swift framework

---
+++ 4_Producer_Swift/Foo/FooFactory.swift
@@ -0,0 +1,15 @@
+//
+//  Bar.swift
+//  Foo
+//
+//  Created by Francis Chong on 12/2/2020.
+//  Copyright © 2020 Francis Chong. All rights reserved.
+//
+
+import Foundation
+
+public class FooFactory {
+    public static func foo() -> String {
+        return "Bar"
+    }
+}
