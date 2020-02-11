5b2894840541d2b459934026ca98a655345f761a
Francis Chong <siu.ying@gmail.com>
2020-02-12 01:35:36 +0800

    add headers

---
+++ 4_Producer_Swift/Frameworks/static/Foo.xcframework/ios-x86_64-maccatalyst/Headers/Foo.h
@@ -0,0 +1,19 @@
+//
+//  Foo.h
+//  Foo
+//
+//  Created by Francis Chong on 12/2/2020.
+//  Copyright © 2020 Francis Chong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Foo.
+FOUNDATION_EXPORT double FooVersionNumber;
+
+//! Project version string for Foo.
+FOUNDATION_EXPORT const unsigned char FooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foo/PublicHeader.h>
+
+
