f1e6242af4875e6644e0882c66bc289a2d41c4ea
Francis Chong <siu.ying@gmail.com>
2020-02-11 19:21:51 +0800

    include static framework

--- 2_Consumer_Static/FrameworkConsumer/AppDelegate.swift
+++ 2_Consumer_Static/FrameworkConsumer/AppDelegate.swift
@@ -7,13 +7,9 @@
 //
 
 import UIKit
-import Sample
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("\(Greeter.greeting())")
